536c2361ef1e79c88e335b9f05e4736b27cfb223
Dv <75692623+kyaruwo@users.noreply.github.com>
2022-12-29 21:42:11 +0800

    Create ps1a.py

---
+++ 0 Intro CS/1 Programming in Python/01 Branching and Iteration/ps1/ps1a.py
@@ -0,0 +1,20 @@
+annual_salary = float(input("annual salary: "))
+portion_saved = float(input("percent of salary to save in decimal: "))
+total_cost = float(input("cost of dream home: "))
+
+monthly_salary = annual_salary/12
+monthly_savings = monthly_salary*portion_saved
+current_savings = 0
+
+r = 0.04
+
+portion_down_payment = total_cost*0.25
+
+number_months = 0
+while current_savings < portion_down_payment:
+    number_months += 1
+
+    roi = current_savings*r/12
+    current_savings += roi + monthly_savings
+
+print(f"Number of Months: {number_months}")
