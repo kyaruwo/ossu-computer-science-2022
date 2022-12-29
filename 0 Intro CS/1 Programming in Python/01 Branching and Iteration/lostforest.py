8d5c3da176c673b5f9b68dd472b0e1d70dc54c33
Dv <75692623+kyaruwo@users.noreply.github.com>
2022-12-29 19:29:25 +0800

    Update lostforest.py

--- 0 Intro CS/1 Programming in Python/01 Branching and Iteration/lostforest.py
+++ 0 Intro CS/1 Programming in Python/01 Branching and Iteration/lostforest.py
@@ -3,19 +3,28 @@ from os import system as sys
 
 def wall():
     wall_length = 26
-    print("-"*wall_length+"\n"+"-"*wall_length)
+    print("*"*wall_length+"\n"+"*"*wall_length)
 
 
+move_count = 1
 while True:
     sys("cls")
     print("You are in the Lost Forest.")
+
+    if move_count != 1:
+        print("*"*26)
+        print("****          ************")
+        print("(╯°□°）╯︵ ┻━┻")
+    else:
+        wall()
+        print("(* ￣︿￣)")
+
     wall()
-    print(">_<")
-    wall()
-    direction = input("Go left or right ? ")
+    direction = input("Go left or nah ? ")
     if direction == "left":
         break
+    move_count += 1
 
 sys("cls")
-print("\n\n\n>_<\n\n")
-print("You got out of the Lost Forest!")
+print("\n\n\n(￣︶￣)\n\n")
+print(f"You got out of the Lost Forest! in {move_count} move")
