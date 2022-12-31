67e4b2ac1f27c4190212337c92408c85437b48ad
Dv <75692623+kyaruwo@users.noreply.github.com>
2022-12-31 10:24:45 +0800

    Rename Folder
    01 Branching and Iteration > Lecture 2

---
+++ 0 Intro CS/1 Programming in Python/Lecture 2/ps1/ps1c.py
@@ -0,0 +1,14 @@
+from os import system as sys
+sys("cls")
+
+annual_salary = float(input("annual salary: "))
+semi_annual_raise = .07
+
+portion_saved = 0
+r = 0.04
+
+total_cost = 1000000
+portion_down_payment = total_cost*0.25
+
+
+# watch lecture 3 video for bisection
