f1ab286a311a3531e1c023082c45cd528369f5dc
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-01-01 13:23:21 +0800

    Create cheer.py

---
+++ 0 Intro CS/1 Programming in Python/Lecture 3/cheer.py
@@ -0,0 +1,23 @@
+an_letters = "aefhilmnorsxAEFHILMNORSX"
+word = input("I will cheer for you! Enter a word: ")
+times = int(input("Enthusiasm level (1-10): "))
+
+# i = 0
+# while i < len(word):
+#     c = word[i]
+#     if c in an_letters:
+#         print("Give me an " + c + "! " + c)
+#     else:
+#         print("Give me a " + c + "! " + c)
+#     i += 1
+
+for c in word:
+    if c in an_letters:
+        print("Give me an " + c + "! " + c)
+    else:
+        print("Give me a " + c + "! " + c)
+
+print("What does that spell?")
+
+for i in range(times):
+    print(word, "!!!")
