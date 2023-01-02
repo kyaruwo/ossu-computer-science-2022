be7f30c54ab9677a394d92cf2d0601970be37bdb
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-01-02 23:14:20 +0800

    Update bisection.py

--- 0 Intro CS/1 Programming in Python/Lecture 3/bisection.py
+++ 0 Intro CS/1 Programming in Python/Lecture 3/bisection.py
@@ -1,20 +1,21 @@
 from os import system as sys
 sys("cls")
 
-# allow negative cubes
-cube = float(input("cube : "))
+
+cube = int(input("cube : "))
+acube = abs(cube)
 
 low = 0
-high = cube
+high = acube
 
-# accuracy
+# accuracy, machine epsilon
 epsilon = 0.1
 
 guess = (high + low) / 2
 
 num_guesses = 0
-while abs(guess**3 - cube) >= epsilon:
-    if guess**3 < cube:
+while abs(guess**3 - acube) >= epsilon:
+    if guess**3 < acube:
         low = guess
     else:
         high = guess
@@ -24,4 +25,7 @@ while abs(guess**3 - cube) >= epsilon:
     num_guesses += 1
 
 print(f"num_guesses : {num_guesses}")
+
+if cube != acube:
+    guess = -guess
 print(f"{guess} is close to the cube root of {cube}")
