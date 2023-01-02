1c6ede8df1cceacad1cff0e5cad48453dc421b32
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-01-03 07:21:18 +0800

    Update guess&check.py

--- 0 Intro CS/1 Programming in Python/Lecture 3/guess&check.py
+++ 0 Intro CS/1 Programming in Python/Lecture 3/guess&check.py
@@ -5,7 +5,6 @@ sys("cls")
 cube = int(input("cube : "))
 acube = abs(cube)
 
-num_guesses = 0
 for guess in range(acube+1):
     if guess**3 >= acube:
         break
@@ -13,9 +12,8 @@ for guess in range(acube+1):
         # any following guess is greater than the cube itself
         #
         # if guess is equal to cube, we found the cuberoot
-    num_guesses += 1
 
-print(f"num_guesses : {num_guesses}")
+print(f"num_guesses : {guess}")
 
 if guess**3 != acube:
     print(f"{cube} is not a perfect cube")
