1a7860e2ad463e825dadc99b29a0591207b68bb4
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-01-02 23:14:17 +0800

    Update approximate_solution.py

--- 0 Intro CS/1 Programming in Python/Lecture 3/approximate_solution.py
+++ 0 Intro CS/1 Programming in Python/Lecture 3/approximate_solution.py
@@ -1,21 +1,26 @@
 from os import system as sys
 sys("cls")
 
-# allow negative cubes
-cube = float(input("cube : "))
 
-# accuracy
+cube = int(input("cube : "))
+acube = abs(cube)
+
+# accuracy, machine epsilon
 epsilon = 0.1
 increment = 0.01
 
 guess = 0
+
 num_guesses = 0
-while abs(guess**3 - cube) >= epsilon and guess <= cube:
+while abs(guess**3 - acube) >= epsilon and guess**3 <= acube:
     guess += increment
     num_guesses += 1
 
 print(f"num_guesses : {num_guesses}")
-if abs(guess**3 - cube) >= epsilon:
+
+if abs(guess**3 - acube) >= epsilon:
     print(f"Failed on cube root of {cube}")
 else:
+    if cube != acube:
+        guess = -guess
     print(f"{guess} is close to the cube root of {cube}")
