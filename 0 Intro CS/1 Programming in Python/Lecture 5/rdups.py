1fb4d18dbfd873855e81be103b55e6380e5c1f37
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-01-18 16:56:40 +0800

    Update rdups.py

--- 0 Intro CS/1 Programming in Python/Lecture 5/rdups.py
+++ 0 Intro CS/1 Programming in Python/Lecture 5/rdups.py
@@ -1,7 +1,7 @@
-def remove_dups(list1, list2):
-    for e in list1:
-        if e in list2:
-            list1.remove(e)
+def remove_dups(list_1, list_2):
+    for e in list_1:
+        if e in list_2:
+            list_1.remove(e)
 
 
 l1 = [1, 2, 3, 4]
@@ -21,7 +21,6 @@ l1 is [2, 3, 4] not [3, 4] why?
     - mutating changes the list length but python doesn't update the counter
     - loop never sees element 2
 
-we can solve this by making a copy of the list first
 
 l1 = [1, 2, 3, 4]
 l2 = [1, 2, 5, 6]
@@ -43,3 +42,31 @@ end of the list
 
 
 """
+
+
+# we can solve this by making a copy of the list and return that mutated copy
+
+
+def remove_dups(list_1, list_2):
+    """
+    \nlist_1 is where we remove duplicate elements
+    \nlist_2 is where we check duplicate elements
+    \nreturns a copy of list_1 where duplicate elements are removed
+    """
+    list_1c = list_1[:]
+    for e in list_1:
+        if e in list_2:
+            list_1c.remove(e)
+    return list_1c
+
+
+l1 = [1, 2, 3, 4]
+l2 = [1, 2, 5, 6]
+
+print(l1, l2)
+# [1, 2, 3, 4] [1, 2, 5, 6]
+
+l1 = remove_dups(l1, l2)
+
+print(l1, l2)
+# [3, 4] [1, 2, 5, 6]
