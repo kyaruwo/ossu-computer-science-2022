e72c91afa8a11828aa2b4200659c04d837941f27
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-01-18 17:32:06 +0800

    Create mult_iter.py

---
+++ 0 Intro CS/1 Programming in Python/Lecture 6/mult_iter.py
@@ -0,0 +1,19 @@
+def mult_iter(a, b):
+    res = 0
+    while b > 0:
+        res += a
+        b -= 1
+    return res
+
+
+print(mult_iter(4, 4))
+
+
+def recursive_mult_iter(a, b):
+    if b > 0:
+        return a + recursive_mult_iter(a, b - 1)
+    else:
+        return 0
+
+
+print(recursive_mult_iter(4, 4))
