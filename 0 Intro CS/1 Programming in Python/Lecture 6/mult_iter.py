f3b98b5603988614c59f25cef213f889a570fa29
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-01-18 19:11:45 +0800

    Update mult_iter.py

--- 0 Intro CS/1 Programming in Python/Lecture 6/mult_iter.py
+++ 0 Intro CS/1 Programming in Python/Lecture 6/mult_iter.py
@@ -9,6 +9,13 @@ def mult_iter(a, b):
 print(mult_iter(4, 4))
 
 
+# def recursive_mult_iter(a, b):
+#     if b == 1:
+#         return a
+#     else:
+#         return a + recursive_mult_iter(a, b - 1)
+#
+# by me
 def recursive_mult_iter(a, b):
     if b > 0:
         return a + recursive_mult_iter(a, b - 1)
