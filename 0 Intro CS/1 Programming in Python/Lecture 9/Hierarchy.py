ef0fdb0015cad729c865875cad4ee8cb7602a984
Dv <75692623+kyaruwo@users.noreply.github.com>
2023-02-02 22:23:47 +0800

    Create Hierarchy.py

---
+++ 0 Intro CS/1 Programming in Python/Lecture 9/Hierarchy.py
@@ -0,0 +1,30 @@
+class Animal(object):
+    def __init__(self, age):
+        self.age = age
+        self.name = None
+
+    def __str__(self):
+        return f"animal: {self.name}:{self.age}"
+
+    # methods
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, new_age):
+        assert type(new_age) == int, "integer only"
+        self.age = new_age
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, new_name=""):
+        assert type(new_name) == str, "string only"
+        self.name = new_name
+
+
+class Cat(Animal):
+    pass
+
+# 23:00
+# https://ocw.mit.edu/courses/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/resources/lecture-9-python-classes-and-inheritance/
